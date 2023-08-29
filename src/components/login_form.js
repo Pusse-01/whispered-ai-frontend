63c4705ffd7e353e73f70943d8bf04732fe6e58d
Nirodya Pussadeniya <en95142@sjp.ac.lk>
2023-08-29 07:52:47 +0530

    updated

--- src/components/login_form.js
+++ src/components/login_form.js
@@ -1,19 +1,58 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import API_BASE_URL from '../config';
+import CircularProgress from '@mui/material/CircularProgress';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
 const LoginForm = ({ handleLogin }) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState({
+        name: '',
+        password: '',
+        newPassword: '',
+        confirmNewPassword: '',
+    });
+    const [errorMessage, setErrorMessage] = useState('');
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
 
     const handleUsernameChange = (event) => {
         setUsername(event.target.value);
+        validateInput(event);
+    };
+
+    const handleSnackbarClose = () => {
+        setSnackbarOpen(false);
     };
 
     const handlePasswordChange = (event) => {
         setPassword(event.target.value);
+        validateInput(event);
     };
+    const validateInput = e => {
+        let { name, value } = e.target;
+        setError(prev => {
+            const stateObj = { ...prev, [name]: "" };
 
+            switch (name) {
+                case "username":
+                    if (!value) {
+                        stateObj[name] = "Please enter your Email.";
+                    }
+                    break;
+
+                case "password":
+                    if (!value) {
+                        stateObj[name] = "Please enter your Password.";
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return stateObj;
+        });
+    }
     const handleFormSubmit = (event) => {
         event.preventDefault();
         // Perform login logic by sending data to the backend
@@ -29,7 +68,9 @@ const LoginForm = ({ handleLogin }) => {
         })
             .then((response) => {
                 if (!response.ok) {
-                    throw new Error('Invalid credentials');
+                    setErrorMessage('Invalid credentials!')
+                    throw new Error('Invalid credentials!');
+
                 }
                 return response.json();
             })
@@ -41,36 +82,50 @@ const LoginForm = ({ handleLogin }) => {
             })
             .catch((error) => {
                 console.error(error.message);
+                setErrorMessage(error.message || 'Login Failed!')
                 // Handle login error (e.g., display error message)
-            });
+            })
+            .finally(() => {
+                setSnackbarOpen(true);
+            })
     };
 
     return (
         <div>
             <form onSubmit={handleFormSubmit} className="mb-8" action="#">
-                <div className="form-control w-full ">
-                    <label className="label">
-                        <span className="form-label label-text">Username</span>
-                    </label>
-                    <input
-                        type="text"
-                        id="username"
-                        value={username}
-                        onChange={handleUsernameChange}
-                        className="my-2 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-md border-gray-300 rounded-md px-2"
-                        required
-                    />
-                    <label className="label">
-                        <span className="form-label label-text">Password</span>
-                    </label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={handlePasswordChange}
-                        className="my-2 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-md border-gray-300 rounded-md px-2"
-                        required
-                    />
+                <div className="form-control w-full text-left">
+                    <div className="items-center mb-4 text-left">
+                        <label className="label">
+                            <span className="form-label label-text ">Email</span>
+                        </label>
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            value={username}
+                            onChange={handleUsernameChange}
+                            onBlur={validateInput}
+                            className="my-2 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-md border-gray-300 rounded-md px-2"
+                            required
+                        />
+                        {error.username && <span className='text-sm text-red-500'>{error.username}</span>}
+                    </div>
+                    <div className='items-center mb-4 text-left'>
+                        <label className="label">
+                            <span className="form-label label-text">Password</span>
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            name='password'
+                            value={password}
+                            onChange={handlePasswordChange}
+                            onBlur={validateInput}
+                            className="my-2 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-md border-gray-300 rounded-md px-2"
+                            required
+                        />
+                        {error.password && <span className='text-sm text-red-500'>{error.password}</span>}
+                    </div>
                     <button className="btn bg-blue-800 btn-block px-2 py-1 rounded-md text-white text-sm">Log in </button>
                 </div>
             </form>
@@ -80,6 +135,28 @@ const LoginForm = ({ handleLogin }) => {
                     Sign up
                 </Link>
             </p>
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={3000}
+                onClose={handleSnackbarClose}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+            >
+                <div>
+                    {errorMessage && (
+                        <div>
+                            <MuiAlert
+                                elevation={6}
+                                variant="filled"
+                                onClose={handleSnackbarClose}
+                                severity="error"
+                            >
+                                {errorMessage}
+                            </MuiAlert>
+                        </div>
+                    )}
+                </div>
+
+            </Snackbar>
         </div>
     );
 };
