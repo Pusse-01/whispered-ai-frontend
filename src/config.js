e52afe14e0ba6188578d54b67210e4cbb9eb50ce
Nirodya Pussadeniya <en95142@sjp.ac.lk>
2023-10-08 12:20:17 +0530

    added the backend base API

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // export const API_BASE_URL = "https://whispered.synacal.ai";
-export const API_BASE_URL = "http://127.0.0.1:8000";
-// export const API_BASE_URL = "https://backend.whispered.ai";
+// export const API_BASE_URL = "http://127.0.0.1:8000";
+export const API_BASE_URL = "https://backend.whispered.ai";
 export const FE_URL = "https://app.whispered.ai"
